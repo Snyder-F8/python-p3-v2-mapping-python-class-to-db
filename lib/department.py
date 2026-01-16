c15c9b37df46462d02061fd38e0223779adff3a6
Snyder-F8 <fridahnzomo857@gmail.com>
2026-01-16 11:57:38 +0300

    Solved code along

--- lib/department.py
+++ lib/department.py
@@ -1,19 +1,14 @@
 from __init__ import CURSOR, CONN
 
-
-# class Department:
-
-#     def __init__(self, name, location, id=None):
-#         self.id = id
-#         self.name = name
-#         self.location = location
-
-#     def __repr__(self):
-#         return f"<Department {self.id}: {self.name}, {self.location}>"
-
 class Department:
 
-    # add new ORM methods after existing methods ....
+    def __init__(self, name, location, id=None):
+        self.id = id
+        self.name = name
+        self.location = location
+
+    def __repr__(self):
+        return f"<Department {self.id}: {self.name}, {self.location}>"
 
     @classmethod
     def create_table(cls):
@@ -36,3 +31,43 @@ class Department:
         CURSOR.execute(sql)
         CONN.commit()
 
+    def save(self):
+        """ Insert a new row with the name and location values of the current Department instance.
+        Update object id attribute using the primary key value of new row.
+        """
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):
+        """ Initialize a new Department instance and save the object to the database """
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        """Update the table row corresponding to the current Department instance."""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        """Delete the table row corresponding to the current Department instance"""
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
